6c66df02fd6a4036f3d5a895264ff7cab86b5ded
Jack Peterson <jlpeterson829@gmail.com>
2019-04-25 10:22:18 -0600

    Setup game timer

--- js/game.js
+++ js/game.js
@@ -1,8 +1,17 @@
 (function() {
+  const GAME_TIME = 60 * 1000;
+
   window.Game = function Game() {
     this.moles = [];
     this.score = 0;
+    this.timeRemaining = GAME_TIME / 1000;
+    // Add an extra second for the GAME_TIME passed into the timer so that we will reach 0 in the countdown.
+    this.timer = new Timer(this.reset.bind(this), GAME_TIME + 1000, this.countdown.bind(this));
+
+    this.timeDisplay = document.getElementById('timeDisplay')
     this.scoreDisplay = document.getElementById('scoreDisplay');
+    this.finalScoreGroupEl = document.getElementById('finalScoreSection')
+    this.finalScoreEl = document.getElementById('finalScore');
     this.gameBoard = document.getElementById('gameBoard');
     this.startButton = document.getElementById('startGame');
     this.stopButton = document.getElementById('stopGame');
@@ -12,6 +21,8 @@
     this.startButton.addEventListener('click', this.start.bind(this));
     this.stopButton.addEventListener('click', this.stop.bind(this));
     this.resetButton.addEventListener('click', this.reset.bind(this));
+
+    this.countdown(true);
   }
 
   Game.prototype.start = function () {
@@ -19,6 +30,7 @@
     this.moles.forEach(function (mole) {
       mole.activate();
     });
+    this.timer.start();
     this.startButton.setAttribute('disabled', 'disabled');
     this.stopButton.removeAttribute('disabled');
     this.resetButton.removeAttribute('disabled');
@@ -28,6 +40,7 @@
     this.moles.forEach(function (mole) {
       mole.stop();
     });
+    this.timer.stop();
     this.startButton.removeAttribute('disabled');
     this.stopButton.setAttribute('disabled', 'disabled');
   }
@@ -37,7 +50,16 @@
       mole.reset();
     });
     this.score = 0;
+    this.timeRemaining = GAME_TIME / 1000;
+    this.timer.stop();
+    this.countdown(true);
     this.renderScore(this.score);
+  }
+
+  Game.prototype.gameOver = function () {
+    this.finalScoreEl.textContent = this.score;
+    this.finalScoreGroupEl.removeAttribute('aria-hidden')
+    this.reset();
   }
 
   Game.prototype.onAttemptedHit = function (e) {
@@ -51,6 +73,12 @@
 
   Game.prototype.renderScore = function () {
     this.scoreDisplay.textContent = this.score;
+  }
+
+  Game.prototype.countdown = function (reset) {
+    if (!reset) this.timeRemaining--;
+    if (this.timeRemaining === 0) return this.gameOver();
+    this.timeDisplay.textContent = this.timeRemaining;
   }
 
   function getMolesFromDOM() {
